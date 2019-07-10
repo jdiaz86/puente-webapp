c3f53b8eee39ef50ed74ac87bbb689cf050918a1
Jorge <jdiaz86@gmail.com>
2019-07-09 22:29:10 -0600

    quick fix on getting roles permissions

--- src/store/user/getters.js
+++ src/store/user/getters.js
@@ -1,15 +1,21 @@
 const getters = {
   isAdmin: (state) => {
     const roles = state.loginUser.roles
-    return roles.filter(role => role.code === 'ADM').length !== 0
+    if (roles) {
+      return roles.filter(role => role.code === 'ADM').length !== 0
+    }
   },
   isTeacher: (state) => {
     const roles = state.loginUser.roles
-    return roles.filter(role => role.code === 'DOC').length !== 0
+    if (roles) {
+      return roles.filter(role => role.code === 'DOC').length !== 0
+    }
   },
   isDonor: (state) => {
     const roles = state.loginUser.roles
-    return roles.filter(role => role.code === 'DON').length !== 0
+    if (roles) {
+      return roles.filter(role => role.code === 'DON').length !== 0
+    }
   },
   userId: (state) => {
     return state.loginUser.id
